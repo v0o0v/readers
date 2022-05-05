8b5c41dd4a454c10ecc2cf839057e08db0c6cbbd
김민석 <v0o0v@gimminseog-ui-MacBookAir.local>
2022-05-05 12:34:24 +0900

    Initial commit

---
+++ src/main/java/hd/readers/ReadersApplication.java
@@ -0,0 +1,13 @@
+package hd.readers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hd/readers/ReadersApplicationTests.java
@@ -0,0 +1,13 @@
+package hd.readers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
